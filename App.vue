5fac30a6061c5327d3db4e87aee5e577950c8170
Augustin Estela <augustinestela@gmail.com>
2023-05-17 20:26:49 +0200

    chat et pong minable

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MessagesDisplay />
+    <PongGame />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MessagesDisplay from './components/MessagesDisplay.vue';
+import PongGame from './components/PongGame.vue';
+
+export default defineComponent({
+  components: {
+    MessagesDisplay,
+    PongGame,
+  },
+});
+</script>
